7a4a81bef42301d0117015754609ca0790c9b8c1
Rukeweazumurana <babyrukkie@gmail.com>
2023-09-08 13:03:52 -0500

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -56,44 +56,6 @@ const Counter = () => {
   );
 };
 
-const Grid = () => {
-  const data = [
-    { name: 'Winter Orbit', rating: 5, id: 1 },
-    { name: 'Dial D for Deadman', rating: 4, id: 2 },
-  ];
 
-  return (
-    <div>
-      <div className="card-container">
-        {data.map((item) => {
-          return (
-            <>
-              <div key={item.id} className="card">
-                <h1>{item.name}</h1>
-                <p>Rating: {item.rating}/5</p>
-                <div>
-                  <Icon.Star />
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
 
-      <div className="grid-container">
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-      </div>
-      <div className="grid-container2">
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-        <div className="grid-item"></div>
-      </div>
-    </div>
-  );
-};
-
-export { Counter, Grid };
+export default Counter;
